050032ee300ef5be76bee482251cecfe979300a2
matie <atiyaleb@gmail.com>
2016-12-13 22:07:53 -0500

    fixes routes

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from "@angular/router";
 import { DashDetailComponent } from './dash-detail/dash-detail.component';
 
 export const DASH_ROUTES: Routes = [
-    { path: '**', component: DashDetailComponent }
+    { path: ':lecture', component: DashDetailComponent }
 ];
